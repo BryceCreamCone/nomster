02848a53ebe46cc39a42dfa3baeba69780231ba8
Bryce Gust <bgust9@gmail.com>
2019-07-15 22:34:57 +0000

    Added more validation to the Place model Really Long story about the commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, length: {minimum: 3, 
+    too_short: "Name does not meet the minimum required characters (#{count})"}
+  validates :name, :address, :description, presence: true
   paginates_per 5
 end
