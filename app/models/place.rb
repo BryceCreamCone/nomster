9a20da3ce0c4d566503eeeb2ba440f0a64556fe7
Bryce Gust <bgust9@gmail.com>
2019-07-16 15:47:54 +0000

    Slightly reorganized Place model code to be more readable

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   validates :name, length: {minimum: 3, 
-    too_short: "Name does not meet the minimum required characters (#{count})"}
-  validates :name, :address, :description, presence: true
+    too_short: "Name does not meet the minimum required characters (%{count})"}, presence: true 
+  validates :address, :description, presence: true
   paginates_per 5
 end
