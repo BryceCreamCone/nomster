c3d01e3a4299be9d29e860092227b5e913dbc594
Bryce Gust <bgust9@gmail.com>
2019-07-17 05:37:29 +0000

    Added comments to database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address 
   after_validation :geocode
+
   validates :name, length: {minimum: 3, 
     too_short: "Name does not meet the minimum required characters (%{count})"}, presence: true 
   validates :address, :description, presence: true
+  
   paginates_per 5
 end
