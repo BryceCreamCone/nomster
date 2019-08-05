f4a938cf793af90f2159d9fe6764868e37ca64dc
Bryce Gust <bgust9@gmail.com>
2019-08-05 21:35:20 +0000

    Made pagination work correctly on users#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_places = Place.where("user_id = ?", @user).page(params[:page]).per(5)
   end
 end
